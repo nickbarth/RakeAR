5e04789701aa28e9a9f9d8dd4d292a750e995ac7
Nick Barth <nickbarth@live.com>
2012-09-23 08:49:52 +0000

    updated rake tasks

--- lib/rake_ar/rake.rb
+++ lib/rake_ar/rake.rb
@@ -3,13 +3,17 @@ Bundler.require
 
 require 'rake_ar'
 
-def rakear
-  @rakear ||= RakeAR.new
+def rake_ar
+  @rake_ar ||= RakeAR.new
 end
 
 namespace :db do
+  task :load_models do
+    rake_ar.load_models
+  end
+
   desc 'Load the console'
-  task :console do
+  task :console => [:load_models] do
     require 'irb'
     require 'irb/completion'
     ARGV.clear
@@ -17,48 +21,48 @@ namespace :db do
   end
 
   desc 'Dump a schema file'
-  task :schema do
-    rakear.dump_schema
+  task :schema => [:load_models] do
+    rake_ar.dump_schema
   end
 
   desc 'Load a schema.rb file into the database'
-  task :load do
-    rakear.load_schema
+  task :load => [:load_models] do
+    rake_ar.load_schema
   end
 
   desc 'Load the seed data'
-  task :seed do
-    rakear.seed_db
+  task :seed => [:load_models] do
+    rake_ar.seed_db
   end
   
   desc 'Clear all database records'
-  task :clear do
-    rakear.clear_db
+  task :clear => [:load_models] do
+    rake_ar.clear_db
   end
 
   desc 'Drop all database tables'
-  task :drop do
-    rakear.drop_db
+  task :drop => [:load_models] do
+    rake_ar.drop_db
   end
 
   desc 'migrate your database'
-  task :migrate do
-    rakear.migrate_db
+  task :migrate => [:load_models] do
+    rake_ar.migrate_db
   end
 
   desc 'create an ActiveRecord migration in ./db/migrate'
-  task :create_migration do
-    rakear.create_migration
+  task :create_migration => [:load_models] do
+    rake_ar.create_migration
   end
 
   desc 'Reload and reseed the database from schema'
-  task :reseed do
+  task :reseed => [:load_models] do
     Rake::Task['db:load'].invoke
     Rake::Task['db:seed'].invoke
   end
 
   desc 'Regenerates the database from migrations'
-  task :regen do
+  task :regen => [:load_models] do
     Rake::Task['db:drop'].invoke
     Rake::Task['db:migrate'].invoke
     Rake::Task['db:schema'].invoke
